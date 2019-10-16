a2d82a5f331c0ae4ae0418b493d9006465990f93
de24world <de24world@gmail.com>
2019-10-16 22:47:50 +0200

    item.component update

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -9,35 +9,36 @@ import { ItemService } from '../services/item.service';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-  private item: Item;
-  private editable: boolean = false;
+  public item: Item;
+  private editable = false;
 
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              private itemService: ItemService) {
-                this.item = {
-                  key: '',
-                  id: '',
-                  name: '',
-                  available: true
-                };  
-              }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private itemService: ItemService
+  ) {
+    this.item = {
+      key: '',
+      id: '',
+      name: '',
+      available: true
+    };
+  }
 
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get('id');
     console.log(id);
 
     //if(id != null){
-    if(id){
-
-        this.editable = true;
-        this.item = this.itemService.getItem(id);
+    if (id) {
+      this.editable = true;
+      this.item = this.itemService.getItem(id);
     }
   }
 
   saveItem() {
     console.log(this.item);
-    if(this.editable) {
+    if (this.editable) {
       //this.itemService.saveItems();
       this.itemService.updateItem(this.item);
     } else {
