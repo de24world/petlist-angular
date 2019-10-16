e143d6d411c6bc4f0b8aa39a6f2e0d95eeaa5e33
de24world <de24world@gmail.com>
2019-10-16 22:44:34 +0200

    enviroment.prod.ts update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD6joZQHj0M6XdE5wx8sGma2DbSADZ-ka4',
+    authDomain: 'petstore-angular.firebaseapp.com',
+    databaseURL: 'https://petstore-angular.firebaseio.com',
+    projectId: 'petstore-angular',
+    storageBucket: 'petstore-angular.appspot.com',
+    messagingSenderId: '1049996128336',
+    appId: '1:1049996128336:web:61fd500f0ddb73219a6155',
+    measurementId: 'G-2V3DQV1955'
+  }
 };
